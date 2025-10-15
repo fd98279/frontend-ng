da0abdec4a5e6921aae03968d9c15989100e61d8
Sampat Ponnaganti <sampatponnaganti@sravz.com>
2025-10-14 22:49:58 -0400

    Add missing SCSS files for feed-card and loader components
    - Added feed-card.component.scss with styling for feed cards
    - Added loader.component.scss with loading spinner animation
    - Updated TypeScript components to reference SCSS files instead of CSS
    - Fixes build errors for missing styleUrls

--- src/app/loader/loader.component.ts
+++ src/app/loader/loader.component.ts
@@ -5,7 +5,7 @@ import { LoaderService } from './loader.service';
 @Component({
   selector: 'app-loader',
   templateUrl: './loader.component.html',
-  styleUrls: ['./loader.component.css']
+  styleUrls: ['./loader.component.scss']
 })
 export class LoaderComponent implements OnInit {
 
